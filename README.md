ab5a7546cc00e747f556f00ea3997583375ddeff
Chris Ying <chrsyng@gmail.com>
2016-06-26 17:13:42 -0700

    zenyatta lines + iteration on existing code

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # Overwatch Voice Responses Reddit Bot
 
-work in progress
+A Reddit bot that scrapes [/r/Overwatch](http://reddit.com/r/overwatch) for hero quotes and responds with the appropriate voice line.
+
+This is currently in beta and is a work in progress. Feel free to send me a pull request if you want to contribute.
+
+## Interesting files:
+
+* `main.py`: the main Python file, run by calling `python main.py`
+* `mapping.json`: map from text to voice data and other info
+* `requirements.txt`: install the requirements via `sudo pip install -r requirements.txt`
+* `local_config.py`: (gitignore'd) contains local information
+
+## Known issues:
+
+* `mapping.json` is incomplete
+* The way I'm handling text normalization will not work for certain emotes (like D.Va's "Winky Face")
+* Basically no error checking at all
+
--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 
-import re
 import json
+import re
 
 import praw
-import OAuth2Util
+#import OAuth2Util
 
 from local_config import *
 
 # Public configs go here
 SUBREDDIT = 'fc_bot_test'
+CHUNK_SIZE = 10
 MAPPING_FILE = 'mapping.json'
-NONALPHANUM = re.compile('[\W_]+')
+NORMALIZE_REGEX = re.compile('[^a-zA-Z0-9 ]')
+COMMENT_TEMPLATE = '''
+[%s](%s) (Sound warning: %s)
 
+-----
+
+^I ^am ^a ^bot! ^Check ^out ^out ^my ^source ^code ^on [^Github](https://github.com/chrisying/overwatch-voice-bot)^! ^I ^am ^still ^in ^beta, ^please ^report ^issues ^by ^messaging ^me ^directly!
+'''
 
 def load_mapping():
     return json.loads(open(MAPPING_FILE).read())
@@ -21,13 +28,13 @@ def ignore_comment(c):
         return True
 
 def normalize_string(s):
-    # TODO: more sophisticated normalization
-    return NONALPHANUM.sub('', s).lower()
+    # Removes punctuation from string and lowercases
+    # TODO: create "special mappings" file which includes punctuation based
+    # voice lines like D.Va's ;) emote
+    return NORMALIZE_REGEX.sub('', s).lower()
 
 def construct_reply(data):
-    # TODO: boilerplate
-    output = ''
-    output += '[%s](%s)' % (data['line'], data['voice'])
+    output = COMMENT_TEMPLATE % (data['line'], data['voice'], data['hero'])
 
     return output
 
@@ -43,13 +50,20 @@ def main():
     #o = OAuth2Util.OAuth2Util(r)
     #o.refresh()
 
+    stream = praw.helpers.comment_stream(r, SUBREDDIT, limit=CHUNK_SIZE, verbosity=0)
+
+    # Ignore first CHUNK_SIZE since they could be duplicate
+    for i in range(CHUNK_SIZE):
+        print 'Ignored: ' + next(stream).body
+
     # Main loop, comment_stream infinitely yields new comments
-    for c in praw.helpers.comment_stream(r, SUBREDDIT, limit=1, verbosity=0):
+    for c in stream:
         if ignore_comment(c):
             continue
 
         normal = normalize_string(c.body)
         if normal in mapping:
+            print 'Responded to comment: %s' % c
             reply = construct_reply(mapping[normal])
             c.reply(reply)
 
